51eb1a3869143514b9c69df65d6ac5c4d55568cb
Szymon Zyguła <szymon@zygula.eu>
2021-10-12 23:34:39 +0200

    Implement Bresenham circle drawing

--- polygon-editor/BresenhamDrawer.cs
+++ polygon-editor/BresenhamDrawer.cs
@@ -90,5 +90,46 @@ namespace polygon_editor
                 }
             }
         }
+
+        private static void PutSymmetricalCirclePixel(UInt32[] pixels, UInt32 color, int width, int x0, int y0, int x, int y)
+        {
+            pixels[x0 + x + (y0 + y) * width] = color;
+            pixels[x0 - x + (y0 + y) * width] = color;
+            pixels[x0 + x + (y0 - y) * width] = color;
+            pixels[x0 - x + (y0 - y) * width] = color;
+            pixels[x0 + y + (y0 + x) * width] = color;
+            pixels[x0 - y + (y0 + x) * width] = color;
+            pixels[x0 + y + (y0 - x) * width] = color;
+            pixels[x0 - y + (y0 - x) * width] = color;
+        }
+
+        public static void Circle(UInt32[] pixels, UInt32 color, int width, int r, int x0, int y0)
+        {
+            int deltaE = 3;
+            int deltaSE = 5 - 2 * r;
+            int d = 1 - r;
+            int x = 0;
+            int y = r;
+
+            PutSymmetricalCirclePixel(pixels, color, width, x0, y0, x, y);
+            while(y > x)
+            {
+                if(d < 0)
+                {
+                    d += deltaE;
+                    deltaE += 2;
+                    deltaSE += 2;
+                }
+                else
+                {
+                    d += deltaSE;
+                    deltaE += 2;
+                    deltaSE += 4;
+                    y -= 1;
+                }
+                x += 1;
+                PutSymmetricalCirclePixel(pixels, color, width, x0, y0, x, y);
+            }
+        }
     }
 }
